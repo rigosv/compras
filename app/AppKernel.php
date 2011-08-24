3e5dfdf89c6df7cd7aaafce331b224d19ad36fbd
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2011-08-24 14:11:59 -0600

    Se agregó seguridad, restringiendo algunos controladores

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,6 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Salud\ComprasBundle\SaludComprasBundle(),
             new jQuery\jQueryBundle\jQueryBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Salud/ComprasBundle/Controller/ProductoController.php
+++ src/Salud/ComprasBundle/Controller/ProductoController.php
@@ -7,12 +7,14 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Salud\ComprasBundle\Entity\Item;
 use Salud\ComprasBundle\Form\ItemType;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 class ProductoController extends Controller {
 
     /**
      * @Route("/producto/list", name="_producto_list")
      * @Template()
+     * @Secure(roles="IS_AUTHENTICATED_FULLY")
      */
     public function listAction() {
         //Creamos una instancia del Entity Manager
@@ -27,6 +29,7 @@ class ProductoController extends Controller {
     /**
      * @Route("/producto/show/{id}", name="_producto_show")
      * @Template()
+     * @Secure(roles="IS_AUTHENTICATED_FULLY")
      */
     public function showAction($id) {
         $em = $this->getDoctrine()->getEntityManager();
@@ -44,43 +47,46 @@ class ProductoController extends Controller {
      * @Route("/producto/edit/{id}", name="_producto_edit", requirements={"id"="\d+"}),
      * @Route("/producto/create", name="_producto_create")
      * @Template()
+     * @Secure(roles="ROLE_USER")
      */
     public function editAction($id=null) {
 
         $em = $this->getDoctrine()->getEntityManager();
         if (isset($id)) {
+            
+            if ( !$this->get('security.context')->isGranted('ROLE_ADMIN'))
+                throw new \Symfony\Component\Security\Core\Exception\AccessDeniedException();
+
             $producto = $em->find('SaludComprasBundle:Item', $id);
             if (!$producto)
-                throw $this->createNotFoundException ("Producto no encontrado");
+                throw $this->createNotFoundException("Producto no encontrado");
         }
         else
             $producto = new Item();
 
         $form = $this->createForm(new ItemType(), $producto);
-        
-        
+
+
         if ($this->getRequest()->getMethod() == 'POST') {
             //Enlazar el formulario con los datos enviados por el usuario
             $form->bindRequest($this->getRequest());
-            
+
             //Validar los datos ingresados en el formulario
-            if ($form->isValid())
-            {
+            if ($form->isValid()) {
                 //Guardar el objeto (aún no en la base de datos)
                 $em->persist($producto);
-                
+
                 //Escribir los cambios a la base de datos
                 $em->flush();
-                
+
                 //Después de guardar exitosamente es recomendable
                 //rediriguir al usuario hacia otra página 
                 // para evitar que los datos se reenvien si refresca la página
-                
-                return $this->redirect($this->generateUrl('_producto_show', 
-                        array('id'=>$producto->getId())));
+
+                return $this->redirect($this->generateUrl('_producto_show', array('id' => $producto->getId())));
             }
         }
-        return array('form' => $form->createView() , 'producto'=>$producto);
+        return array('form' => $form->createView(), 'producto' => $producto);
     }
 
 }
