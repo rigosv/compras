e92bcc81db6484a5ac1ab14fffe38570bdd40667
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2011-09-27 22:41:41 -0600

    Se agregó la clase repositorio para la entidad Especifico

--- src/Salud/ComprasBundle/Form/ItemType.php
+++ src/Salud/ComprasBundle/Form/ItemType.php
@@ -5,54 +5,44 @@ namespace Salud\ComprasBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
 
-class ItemType extends AbstractType
-{
-    public function buildForm(FormBuilder $builder, array $options)
-    {
+class ItemType extends AbstractType {
+
+    public function buildForm(FormBuilder $builder, array $options) {
         $builder
-            ->add('descripcionitem', 'text', array('attr'=>array('size'=>50), 'label'=> 'Descripción'))
-            ->add('autorizado', 'checkbox', array('required' => false))
-            ->add('descontinuado', 'checkbox', array('required' => false))
-            ->add('preciounitario', 'money', array('label'=>'Precio Unitario', 
-                                            'currency'=> 'USD'))
-            ->add('bloqueado', 'checkbox', array('required'=> false))
-            ->add('observaciones', 'textarea', array('required'=>false))
-            ->add('idEspecifico', 'entity',
-                    array('class' => 'SaludComprasBundle:Especifico',
-                        'query_builder' => function ($repository) {
-                            return $repository->createQueryBuilder('e')
-                                    ->orderBy('e.descripcionespecifico')
-                                    ->join('e.idCatalogoProducto', 'c')
-                                    ->where('c.codigocatalogo = :codigo_catalogo')
-                                    ->setParameter('codigo_catalogo', '02')
-                                    ;
-                        }
-                    ))
-            ->add('idEspecificoOnu', 'entity',
-                    array('class' => 'SaludComprasBundle:Especifico',
-                        'query_builder' => function ($repo) {
-                            return $repo->createQueryBuilder('e')
-                                    ->orderBy('e.descripcionespecifico')
-                                    ->join('e.idCatalogoProducto', 'c')
-                                    ->where('c.codigocatalogo = :codigo_catalogo')
-                                    ->setParameter('codigo_catalogo', '01')
-                                    ;
-                        }
+                ->add('descripcionitem', 'text', array('attr' => array('size' => 50), 'label' => 'Descripción'))
+                ->add('autorizado', 'checkbox', array('required' => false))
+                ->add('descontinuado', 'checkbox', array('required' => false))
+                ->add('preciounitario', 'money', array('label' => 'Precio Unitario',
+                    'currency' => 'USD'))
+                ->add('bloqueado', 'checkbox', array('required' => false))
+                ->add('observaciones', 'textarea', array('required' => false))
+                ->add('idEspecifico', 'entity', 
+                        array('class' => 'SaludComprasBundle:Especifico',
+                    'query_builder' => function ($repository) {
+                        return $repository->getByCatalogo('02');
+                    },
+                    'label' => 'Específico Catálogo Egresos'
+                ))
+                ->add('idEspecificoOnu', 'entity', array('class' => 'SaludComprasBundle:Especifico',
+                    'query_builder' => function ($repo) {
+                        return $repo->getByCatalogo('01');
+                    },
+                            'label' => 'Específico Catálogo ONU'
                         )
-                    )
-            ->add('idUnidadMedida', 'entity', 
-                    array('class' => 'SaludComprasBundle:UnidadMedida',
-                        'query_builder' => function ($repository){
-                            return $repository->createQueryBuilder('um')
-                                    ->orderBy('um.descripcionunidadmedida');
-                        }
+                )
+                ->add('idUnidadMedida', 'entity', array('class' => 'SaludComprasBundle:UnidadMedida',
+                    'query_builder' => function ($repository) {
+                        return $repository->createQueryBuilder('um')
+                                ->orderBy('um.descripcionunidadmedida');
+                    },
+                    'label'=> 'Unidad de medida'
                         )
-                    )
+                )
         ;
     }
 
-    public function getName()
-    {
+    public function getName() {
         return 'salud_comprasbundle_itemtype';
     }
+
 }
