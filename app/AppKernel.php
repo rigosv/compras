c09aaf57f91232b75bd0dd1e9122006cb92887b9
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2011-10-18 17:00:06 -0600

    Instalación de SonataAdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -3,10 +3,9 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
-{
-    public function registerBundles()
-    {
+class AppKernel extends Kernel {
+
+    public function registerBundles() {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
@@ -20,6 +19,10 @@ class AppKernel extends Kernel
             new Salud\ComprasBundle\SaludComprasBundle(),
             new jQuery\jQueryBundle\jQueryBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Sonata\BluePrintBundle\SonataBluePrintBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
@@ -32,8 +35,8 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    public function registerContainerConfiguration(LoaderInterface $loader) {
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
     }
+
 }
--- app/autoload.php
+++ app/autoload.php
@@ -15,6 +15,11 @@ $loader->registerNamespaces(array(
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
     'FOS'              => __DIR__.'/../vendor/bundles',
+    'Sonata'           => __DIR__.'/../vendor/bundles',
+    'Knp'              => array(
+                            __DIR__.'/../vendor/bundles',
+                            __DIR__.'/../vendor/knp/menu/src',
+                            ),
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
