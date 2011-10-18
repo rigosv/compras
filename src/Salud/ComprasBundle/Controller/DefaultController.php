e2488afe2888fb45824c4e7d8b2897c1a86d32bc
Rigoberto Reyes <rigoberto.reyes@gmail.com>
2011-10-18 15:40:50 -0600

    Customización de FOSUserBundle y se activó la internacionalización translator:      { fallback: %locale% }

--- src/Salud/ComprasBundle/Controller/DefaultController.php
+++ src/Salud/ComprasBundle/Controller/DefaultController.php
@@ -9,11 +9,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return array();
     }
 }
